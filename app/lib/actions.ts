3b38ad94cce42598522d87ff2f5e2d6cf5ce0e64
PetarIvanov01 <123456789petar45@gmail.com>
2024-05-13 20:19:32 +0300

    create authenticate action that calls signIn from /auth file and passes formData

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { sql } from '@vercel/postgres';
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -19,7 +21,7 @@ const FormSchema = z.object({
   date: z.string(),
 });
 
-export type State = {
+type State = {
   errors?: {
     customerId?: string[];
     amount?: string[];
@@ -108,5 +110,24 @@ export async function deleteInvoice(id: string) {
     return { message: 'Deleted Invoice.' };
   } catch (error) {
     return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
